bf3ef06f0b292247772dd2ca28a824ac91725206
Yuriy Ivanyuk <ivanyyuk@gmail.com>
2016-06-29 12:18:08 -0400

    added add/remove and itinerary funcs

--- public/js/app.js
+++ public/js/app.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
 
     var newDay = 'day' + currentIndex;
 
-    var template = '<div class="panel-body ' + newDay + ' hidden" id="itinerary"><div><h4>My Hotel</h4><ul class="list-group listHotels" id="' + newDay + 'Hotels"></ul></div><div><h4>My Restaurants</h4><ul class="list-group listRestaurants" id="currentRestaurants"></ul></div><div><h4>My Activities</h4><ul class="list-group listActivities" id="currentActivities"></ul></div></div>';
+    var template = '<div class="panel-body ' + newDay + ' hidden"><div><h4>My Hotel</h4><ul class="list-group listHotels" id="' + newDay + 'Hotels"></ul></div><div><h4>My Restaurants</h4><ul class="list-group listRestaurants" id="currentRestaurants"></ul></div><div><h4>My Activities</h4><ul class="list-group listActivities" id="currentActivities"></ul></div></div>';
 
     $('#itinerary').append(template);
 
@@ -46,20 +46,62 @@ $(document).ready(function(){
 });
 //change current-day class to button clicked
 $('.day-buttons').on('click', '.btn', function (event) {
+  var $target = $(event.target);
+
   //remove current day class and add it to target clicked unless target was day add button
-  if (!( $(event.target).is('#day-add') )){
+  
+  if (!( $target.is('#day-add') )){
     $('.current-day').removeClass('current-day');
-    $(event.target).addClass('current-day');
+    $target.addClass('current-day');
 
     $('.current').removeClass('current').addClass('hidden');
 
-    var newDay = '.day' + $(event.target).text();
+    var newDay = '.day' + $target.text();
 
     $(newDay).removeClass('hidden').addClass('current');
-
+    var currentDay = 'Day ' + $target.text();
+    $('#day-span').text(currentDay)
   }
 });
 
+ $('#day-title').find('.remove').on('click', function () {
+  var dayArray = $('#day-span').text().toLowerCase().split(' ');
+  var dayNumber = dayArray[1];
+  var removingDay = dayArray.join('');
+
+  $('.' + removingDay).remove();
+  var allButtons = $('.day-buttons').children(); 
+
+  if (allButtons.length === 2){
+    var newDay = 1;
+    var template = '<div class="panel-body day' + newDay + ' current"><div><h4>My Hotel</h4><ul class="list-group listHotels" id="' + newDay + 'Hotels"></ul></div><div><h4>My Restaurants</h4><ul class="list-group listRestaurants" id="currentRestaurants"></ul></div><div><h4>My Activities</h4><ul class="list-group listActivities" id="currentActivities"></ul></div></div>';
+    $('.day1').remove();
+    $('#itinerary').append(template);
+
+  }else {
+    for (var i = 0; i < allButtons.length; i ++) {
+      var button = $(allButtons[i]);
+      if (button.text() === dayNumber){
+        button.remove();
+      if (i > 0) $(allButtons[i-1]).addClass('current-day');
+      else if (i === 0) $(allButtons[i+1]).addClass('current-day');
+      }
+    }
+
+    for (var z = +dayNumber; z < allButtons.length-1; z++){
+      var button = $(allButtons[z]);
+      var buttonText = +(button.text()) - 1;
+      button.text(buttonText);
+      var $targetPanel = $('.day' + z);
+      $targetPanel.find('#day' + z + 'Hotels').removeAttr('id').attr('id', 'day' + (z-1) + 'Hotels');
+      $targetPanel.find('#day' + z + 'Restaurants').removeAttr('id').attr('id', 'day' + (z-1) + 'Restaurants');
+      $targetPanel.find('#day' + z + 'Activities').removeAttr('id').attr('id', 'day' + (z-1) + 'Activities');
+      $targetPanel.removeClass('day' + z).addClass('day'+ (z-1)); 
+    }
+
+    $('.current-day').trigger('click');
+  }
+});
 
 
 
