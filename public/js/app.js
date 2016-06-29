a3359edcc62365ebe25bba630f99565b6f3ed8ff
Jason Wang <jasonwang67@gmail.com>
2016-06-29 11:29:43 -0400

    working panels

--- public/js/app.js
+++ public/js/app.js
@@ -7,42 +7,56 @@ $(document).ready(function(){
   $('#currentActivities').on('click', '.remove' ,function(){
     $(this).parent().remove();
   });
-//remove hotels 
+//remove hotels
   $('#currentHotels').on('click', '.remove' ,function(){
     $(this).parent().remove();
   });
 //add hotels
   $('#hotelButton').on('click', function(){
     var hotel = $('#hotel-choices').val();
-    $('#currentHotels').append('<div class="itinerary-item"><span class="title">' + hotel + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+    $('.current').find('.listHotels').append('<div class="itinerary-item"><span class="title">' + hotel + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
   });
 //add restaurants
   $('#restaurantButton').on('click', function(){
     var restaurant = $('#restaurant-choices').val();
-    $('#currentRestaurants').append('<div class="itinerary-item"><span class="title">' + restaurant + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+    $('.current').find('.listRestaurants').append('<div class="itinerary-item"><span class="title">' + restaurant + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
   });
 //add activities
   $('#activityButton').on('click', function(){
     var activity = $('#activity-choices').val();
-    $('#currentActivities').append('<div class="itinerary-item"><span class="title">' + activity + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
+    $('.current').find('.listActivities').append('<div class="itinerary-item"><span class="title">' + activity + '</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>');
   });
-//add day buttons 
+//add day buttons
   $('#day-add').on('click', function () {
     //get the number of the about to be created button
     var siblingsArray = $(this).siblings();
     var lastIndex = $(siblingsArray[siblingsArray.length - 1]).text();
     var currentIndex = +lastIndex + 1;
-    //add button 
+    //add button
     $('<button class = "btn btn-circle day-btn">' + currentIndex + '</button>').insertBefore(this).css('margin', '2px');
-    $(this).css('margin-left', '2px')
+    $(this).css('margin-left', '2px');
+
+    var newDay = 'day' + currentIndex;
+
+    var template = '<div class="panel-body ' + newDay + ' hidden" id="itinerary"><div><h4>My Hotel</h4><ul class="list-group listHotels" id="' + newDay + 'Hotels"></ul></div><div><h4>My Restaurants</h4><ul class="list-group listRestaurants" id="currentRestaurants"></ul></div><div><h4>My Activities</h4><ul class="list-group listActivities" id="currentActivities"></ul></div></div>';
+
+    $('#itinerary').append(template);
+
   });
 });
 //change current-day class to button clicked
-$('.day-buttons').on('click', function (event) {
-  //remove current day class and add it to target clicked unless target was day add button 
+$('.day-buttons').on('click', '.btn', function (event) {
+  //remove current day class and add it to target clicked unless target was day add button
   if (!( $(event.target).is('#day-add') )){
     $('.current-day').removeClass('current-day');
     $(event.target).addClass('current-day');
+
+    $('.current').removeClass('current').addClass('hidden');
+
+    var newDay = '.day' + $(event.target).text();
+
+    $(newDay).removeClass('hidden').addClass('current');
+
   }
 });
 
