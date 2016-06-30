1ef5c43751845ec8fb97d647bf2c0cf8174861c2
Yuriy Ivanyuk <ivanyyuk@gmail.com>
2016-06-30 12:09:32 -0400

    more server side

--- routes/api/days.js
+++ routes/api/days.js
@@ -1,12 +1,12 @@
-var express = require('express');
+var express = require ('express');
 var router = express.Router();
-var Day = require('../../models/day');
-var Place = require('../../models/place');
-var Activity = require('../../models/activity');
-var Restaurant = require('../../models/restaurant');
-var Hotel = require('../../models/hotel'); 
+var Day = require ('../../models/day');
+var Place = require ('../../models/place');
+var Activity = require ('../../models/activity');
+var Restaurant = require ('../../models/restaurant');
+var Hotel = require ('../../models/hotel'); 
 
-router.get('/days', function(req,res,next) {
+router.get('/days', function(req, res, next) {
   Day.findAll({
     include :[Hotel]
   }).then(function(result){
@@ -14,13 +14,24 @@ router.get('/days', function(req,res,next) {
   })
 })
 
-router.post('/days', function(req,res,next){
+router.post('/days', function(req, res, next){
   var dayObject = req.body;
 
   Day.create(dayObject)
-    .then(function() {
-      res.end();
-    })
+  .then(function() {
+    res.end();
+  })
+})
+
+router.put('/days/:id/hotel', function(req, res, next){
+  var hotelId = (req.body.hotelId).replace(/[^\d.]/g, '');
+  console.log(hotelId)
+  // var id = req.params.id;
+  // Day.findById(id)
+  //   .then(function(result){
+  //     result.update()
+  //   })
+  res.end()
 })
 
 module.exports = router;
